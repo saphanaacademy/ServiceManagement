2fcc59e18d3b517d425dcbe5811b3f718e4b5369
Andrew Lunde <andrew.lunde@sap.com>
2020-09-19 04:52:58 -0400

    Multi Containers

--- ServiceManagement_plugin.go
+++ ServiceManagement_plugin.go
@@ -31,6 +31,11 @@ func handleError(err error) {
 	}
 }
 
+type Containers struct {
+	ContainerID string
+	TenantID    string
+}
+
 func (c *ServiceManagementPlugin) Run(cliConnection plugin.CliConnection, args []string) {
 
 	// flags
@@ -38,6 +43,8 @@ func (c *ServiceManagementPlugin) Run(cliConnection plugin.CliConnection, args [
 	serviceOfferingName := flags.String("offering", "hana", "Service offering")
 	servicePlanName := flags.String("plan", "hdi-shared", "Service plan")
 	showCredentials := flags.Bool("credentials", false, "Show credentials")
+	includeMeta := flags.Bool("meta", false, "Include Meta containers")
+	includeOwner := flags.Bool("owner", false, "Include Owner credentials")
 	outputFormat := flags.String("o", "Txt", "Show as JSON | SQLTools | Txt)")
 	err := flags.Parse(args[1:])
 	handleError(err)
@@ -227,25 +234,14 @@ func (c *ServiceManagementPlugin) Run(cliConnection plugin.CliConnection, args [
 				numItems, err = jsonparser.GetInt(body4Bytes, "num_items")
 				handleError(err)
 
-				switch outputFormat {
-				case "json":
-					fmt.Printf(`{"service_offering": "%s", "service_plan": "%s", "num_items": %d, "items": [`, serviceOfferingName, servicePlanName, numItems)
-				case "sqltools":
-					fmt.Printf(`"sqltools.connections": [`)
-				case "txt":
-					fmt.Printf("%d items found for service offering %s and service plan %s.\n", numItems, serviceOfferingName, servicePlanName)
-				}
+				foundContainers := []Containers{}
 
 				// for each item
 				var item = 0
+				var isMeta = false
 				jsonparser.ArrayEach(body4Bytes, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-					item = item + 1
+					isMeta = false
 					id, _ := jsonparser.GetString(value, "id")
-					name, _ := jsonparser.GetString(value, "name")
-					createdAt, _ := jsonparser.GetString(value, "created_at")
-					updatedAt, _ := jsonparser.GetString(value, "updated_at")
-					ready, _ := jsonparser.GetBoolean(value, "ready")
-					usable, _ := jsonparser.GetBoolean(value, "usable")
 
 					// get service binding
 					url5, err := jsonparser.GetString([]byte(strings.Join(serviceKey, "")), "sm_url")
@@ -261,61 +257,152 @@ func (c *ServiceManagementPlugin) Run(cliConnection plugin.CliConnection, args [
 					defer res5.Body.Close()
 					body5Bytes, err := ioutil.ReadAll(res5.Body)
 					handleError(err)
-					host, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "host")
-					port, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "port")
-					driver, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "driver")
-					schema, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "schema")
-					certificate, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "certificate")
-					re := regexp.MustCompile(`\n`)
-					certificate = re.ReplaceAllString(certificate, "")
-					url, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "url")
-					user, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "user")
-					password, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "password")
-					var hdiuser = ""
-					var hdipassword = ""
-					if servicePlanName == "hdi-shared" {
-						hdiuser, _ = jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "hdi_user")
-						hdipassword, _ = jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "hdi_password")
+
+					tenantID, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "labels", "tenant_id", "[0]")
+					var splits = strings.Split(tenantID, "-")
+					if splits[0] == "TENANT" {
+						isMeta = true
 					}
 
-					if outputFormat == "json" {
-						if item > 1 {
-							fmt.Printf(`,`)
-						}
-						fmt.Printf(`{"name": "%s", "id": "%s", "created_at": "%s", "updated_at": "%s", "ready": %t, "usable": %t, "schema": "%s", "host": "%s", "port": "%s", "url": "%s", "driver": "%s"`, name, id, createdAt, updatedAt, ready, usable, schema, host, port, url, driver)
-						if *showCredentials {
-							fmt.Printf(`, "user": "%s", "password": "%s", "certificate": "%s"`, user, password, certificate)
-							if servicePlanName == "hdi-shared" {
-								fmt.Printf(`, "hdi_user": "%s", "hdi_password": "%s"`, hdiuser, hdipassword)
-							}
-						}
-						fmt.Printf(`}`)
-					} else if outputFormat == "sqltools" {
-						if item > 1 {
-							fmt.Printf(`,`)
-						}
-						fmt.Printf(`{"name": "%s", "dialect": "SAPHana", "server": "%s", "port": %s, "database": "%s", "username": "%s", "password": "%s", "connectionTimeout": 30, "hanaOptions": {"encrypt": true, "sslValidateCertificate": true, "sslCryptoProvider": "openssl", "sslTrustStore": "%s"}},`, serviceManagerName+":"+name, host, port, schema, user, password, certificate)
-						if servicePlanName == "hdi-shared" {
-							fmt.Printf(`{"name": "%s", "dialect": "SAPHana", "server": "%s", "port": %s, "database": "%s", "username": "%s", "password": "%s", "connectionTimeout": 30, "hanaOptions": {"encrypt": true, "sslValidateCertificate": true, "sslCryptoProvider": "openssl", "sslTrustStore": "%s"}}`, serviceManagerName+":"+name+":OWNER", host, port, schema, hdiuser, hdipassword, certificate)
-						}
+					if !isMeta || (isMeta && *includeMeta) {
+						//fmt.Printf("%d: %s \n", item, tenantID)
+						container := Containers{ContainerID: id, TenantID: tenantID}
+						foundContainers = append(foundContainers, container)
+						item = item + 1
+					}
+				}, "items")
+
+				whichID := "ALL"
+
+				if len(foundContainers) > 1 {
+					fmt.Printf("%d: %s \n", 0, "Include All")
+					for i := 0; i < len(foundContainers); i++ {
+						fmt.Printf("%d: %s \n", i+1, foundContainers[i].TenantID)
+					}
+
+					fmt.Print("Which container?: ")
+					var input string
+					fmt.Scanln(&input)
+					cidx, _ := strconv.Atoi(input)
+					if cidx == 0 {
+						fmt.Printf("Using: %s \n", "All Containers")
 					} else {
-						//txt
-						fmt.Printf("\nName: %s \nId: %s \nCreatedAt: %s \nUpdatedAt: %s \nReady: %t \nUsable: %t \nSchema: %s \nHost: %s \nPort: %s \nURL: %s \nDriver: %s\n", name, id, createdAt, updatedAt, ready, usable, schema, host, port, url, driver)
-						if *showCredentials {
-							fmt.Printf("User: %s \nPassword: %s \nCertificate: %s \n", user, password, certificate)
-							if servicePlanName == "hdi-shared" {
-								fmt.Printf("HDIUser: %s \nHDIPassword: %s \n", hdiuser, hdipassword)
+						whichContainer := foundContainers[cidx-1].TenantID
+						fmt.Printf("Using: %s \n", whichContainer)
+						whichID = foundContainers[cidx-1].ContainerID
+					}
+				} else {
+					whichID = foundContainers[0].ContainerID
+				}
+
+				switch outputFormat {
+				case "json":
+					fmt.Printf(`{"service_offering": "%s", "service_plan": "%s", "num_items": %d, "items": [`, serviceOfferingName, servicePlanName, numItems)
+				case "sqltools":
+					fmt.Printf(`{"sqltools.connections": [`)
+				case "txt":
+					fmt.Printf("%d items found for service offering %s and service plan %s.\n", numItems, serviceOfferingName, servicePlanName)
+				}
+
+				// for each item
+				item = 0
+				isMeta = false
+				jsonparser.ArrayEach(body4Bytes, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+					//item = item + 1
+					isMeta = false
+					id, _ := jsonparser.GetString(value, "id")
+					name, _ := jsonparser.GetString(value, "name")
+					createdAt, _ := jsonparser.GetString(value, "created_at")
+					updatedAt, _ := jsonparser.GetString(value, "updated_at")
+					ready, _ := jsonparser.GetBoolean(value, "ready")
+					usable, _ := jsonparser.GetBoolean(value, "usable")
+
+					if (whichID == id) || (whichID == "ALL") {
+
+						// get service binding
+						url5, err := jsonparser.GetString([]byte(strings.Join(serviceKey, "")), "sm_url")
+						handleError(err)
+						req5, err := http.NewRequest("GET", url5+"/v1/service_bindings", nil)
+						handleError(err)
+						q5 := req5.URL.Query()
+						q5.Add("fieldQuery", "service_instance_id eq '"+id+"'")
+						req5.URL.RawQuery = q5.Encode()
+						req5.Header.Set("Authorization", "Bearer "+accessToken)
+						res5, err := cli.Do(req5)
+						handleError(err)
+						defer res5.Body.Close()
+						body5Bytes, err := ioutil.ReadAll(res5.Body)
+						handleError(err)
+						host, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "host")
+						port, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "port")
+						driver, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "driver")
+						schema, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "schema")
+						certificate, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "certificate")
+						re := regexp.MustCompile(`\n`)
+						certificate = re.ReplaceAllString(certificate, "")
+						url, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "url")
+						user, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "user")
+						password, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "password")
+						var hdiuser = ""
+						var hdipassword = ""
+						if servicePlanName == "hdi-shared" {
+							hdiuser, _ = jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "hdi_user")
+							hdipassword, _ = jsonparser.GetString(body5Bytes, "items", "[0]", "credentials", "hdi_password")
+						}
+						tenantID, _ := jsonparser.GetString(body5Bytes, "items", "[0]", "labels", "tenant_id", "[0]")
+						var splits = strings.Split(tenantID, "-")
+						if splits[0] == "TENANT" {
+							isMeta = true
+						}
+
+						//name = serviceManagerName + ":" + tenantID
+						//name = tenantID
+
+						// Need to use the SAPCP API to get the subdomain from the subaccount GUID which is the tenantID
+						// sapcp get accounts/subaccount b44f32d4-6e31-4d95-b17f-6c6fcdb37e1f
+
+						if !isMeta || (isMeta && *includeMeta) {
+							item = item + 1
+							if outputFormat == "json" {
+								if item > 1 {
+									fmt.Printf(`,`)
+								}
+								fmt.Printf(`{"name": "%s", "id": "%s", "created_at": "%s", "updated_at": "%s", "ready": %t, "usable": %t, "schema": "%s", "host": "%s", "port": "%s", "url": "%s", "driver": "%s"`, name, id, createdAt, updatedAt, ready, usable, schema, host, port, url, driver)
+								if *showCredentials {
+									fmt.Printf(`, "user": "%s", "password": "%s", "certificate": "%s"`, user, password, certificate)
+									if servicePlanName == "hdi-shared" && *includeOwner {
+										fmt.Printf(`, "hdi_user": "%s", "hdi_password": "%s"`, hdiuser, hdipassword)
+									}
+								}
+								fmt.Printf(`}`)
+							} else if outputFormat == "sqltools" {
+								if item > 1 {
+									fmt.Printf(`,`)
+								}
+								fmt.Printf(`{"name": "%s", "dialect": "SAPHana", "server": "%s", "port": %s, "database": "%s", "username": "%s", "password": "%s", "connectionTimeout": 30, "hanaOptions": {"encrypt": true, "sslValidateCertificate": true, "sslCryptoProvider": "openssl", "sslTrustStore": "%s"}}`, serviceManagerName+":"+tenantID, host, port, schema, user, password, certificate)
+								if servicePlanName == "hdi-shared" && *includeOwner {
+									fmt.Printf(`,{"name": "%s", "dialect": "SAPHana", "server": "%s", "port": %s, "database": "%s", "username": "%s", "password": "%s", "connectionTimeout": 30, "hanaOptions": {"encrypt": true, "sslValidateCertificate": true, "sslCryptoProvider": "openssl", "sslTrustStore": "%s"}}`, serviceManagerName+":"+tenantID+":OWNER", host, port, schema, hdiuser, hdipassword, certificate)
+								}
+							} else {
+								//txt
+								fmt.Printf("\nName: %s \nId: %s \nCreatedAt: %s \nUpdatedAt: %s \nReady: %t \nUsable: %t \nSchema: %s \nHost: %s \nPort: %s \nURL: %s \nDriver: %s\n", name, id, createdAt, updatedAt, ready, usable, schema, host, port, url, driver)
+								if *showCredentials {
+									fmt.Printf("User: %s \nPassword: %s \nCertificate: %s \n", user, password, certificate)
+									if servicePlanName == "hdi-shared" && *includeOwner {
+										fmt.Printf("HDIUser: %s \nHDIPassword: %s \n", hdiuser, hdipassword)
+									}
+								}
+								fmt.Printf("TenantID: %s \n", tenantID)
 							}
 						}
 					}
-
 				}, "items")
 
 				switch outputFormat {
 				case "json":
 					fmt.Println(`]}`)
 				case "sqltools":
-					fmt.Println(`]`)
+					fmt.Println(`]}`)
 				}
 			}
 
@@ -333,7 +420,7 @@ func (c *ServiceManagementPlugin) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 1,
 			Minor: 0,
-			Build: 3,
+			Build: 5,
 		},
 		MinCliVersion: plugin.VersionType{
 			Major: 6,
@@ -346,9 +433,11 @@ func (c *ServiceManagementPlugin) GetMetadata() plugin.PluginMetadata {
 				Alias:    "sm",
 				HelpText: "Show service manager service instances for a service offering and plan.",
 				UsageDetails: plugin.Usage{
-					Usage: "cf service-manager-service-instances [SERVICE_MANAGER_INSTANCE] [-offering <SERVICE_OFFERING>] [-plan <SERVICE_PLAN>] [--credentials] [-o JSON | SQLTools | Txt]",
+					Usage: "cf service-manager-service-instances [SERVICE_MANAGER_INSTANCE] [-offering <SERVICE_OFFERING>] [-plan <SERVICE_PLAN>] [--credentials] [--meta] [--owner] [-o JSON | SQLTools | Txt]",
 					Options: map[string]string{
 						"credentials": "Show credentials",
+						"meta":        "Include Meta containers",
+						"owner":       "Include Owner credentials",
 						"o":           "Show as JSON | SQLTools | Txt (default 'Txt')",
 						"offering":    "Service offering (default 'hana')",
 						"plan":        "Service plan (default 'hdi-shared')",
